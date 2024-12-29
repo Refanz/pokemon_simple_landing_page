dc2dbc12f136adbb74a0c52ccde9e2edf831e7a1
Refanz <refandasuryasaputra@gmail.com>
2024-12-29 17:01:42 +0700

    add image slide show

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
-let slideIndex = 1;
+let slideIndex = -1;
+
+const runAutomaticSlides = () => {
+    let imgSlides = document.querySelectorAll(".jumbotron-img img");
+
+    for (let i = 0; i < imgSlides.length; i++) {
+        imgSlides[i].className = imgSlides[i].className.replace("active", "");
+    }
+
+    slideIndex++;
+
+    if (slideIndex === 3) {
+        slideIndex = 0;
+    }
+
+    imgSlides[slideIndex].className += "active";
+    setTimeout(runAutomaticSlides, 2000);
+}
+
+runAutomaticSlides();
 
 const currentSlide = (num) => {
-    showImageSlide(num);
+    showImageSlide(slideIndex = num - 1);
 }
 
 const showImageSlide = (num) => {
@@ -11,5 +30,5 @@ const showImageSlide = (num) => {
         imgSlides[i].className = imgSlides[i].className.replace("active", "");
     }
 
-    imgSlides[num - 1].className.a
+    imgSlides[num].className += "active";
 }
